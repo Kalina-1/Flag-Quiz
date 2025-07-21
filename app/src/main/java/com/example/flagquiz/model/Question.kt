55b4cdad2d3230c8c39e226551d09aa4aee19a01
Jashmina <jashminathapa0@gmail.com>
2025-07-21 18:37:35 +0545

    Update

--- app/src/main/java/com/example/flagquiz/model/Question.kt
+++ app/src/main/java/com/example/flagquiz/model/Question.kt
@@ -1,9 +1,8 @@
-
 package com.example.flagquiz.model
 
 data class Question(
-    val flagImage: Int,    // Flag image resource
-    val options: List<String>,   // List of options
-    val correctAnswer: String,   // Correct answer
-    val capitalHint: String     // Capital city hint
+    val flagImage: Int,
+    val options: List<String>,
+    val correctAnswer: String,
+    val capitalHint: String
 )
