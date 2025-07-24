641481db4a9384547526768f04db3c3cfe293246
Jashmina <jashminathapa0@gmail.com>
2025-07-24 15:34:04 +0545

    Updated model

--- app/src/main/java/com/example/flagquiz/model/Question.kt
+++ app/src/main/java/com/example/flagquiz/model/Question.kt
@@ -1,5 +1,7 @@
-package com.example.flagquiz.model
-
+// This class holds all the info for a quiz question:
+// the flag image resource, a list of answer options,
+// the correct answer itself, and a little hint about the capital city.
+// Makes it easy to pass around all question details together.
 data class Question(
     val flagImage: Int,
     val options: List<String>,
