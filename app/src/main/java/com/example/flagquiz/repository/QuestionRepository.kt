ce0b3fa2601224733fdf7aeb86867f996eb01a42
Kalina-1 <kalinashrestha16@gmail.com>
2025-07-24 15:39:06 +0545

    updated question repo

--- app/src/main/java/com/example/flagquiz/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/flagquiz/repository/QuestionRepository.kt
@@ -3,7 +3,8 @@ package com.example.flagquiz.repository
 
 import com.example.flagquiz.model.Question
 
-
+// Interface to get questions —
+// will have different implementations depending on where questions come from (API, local, etc.)
 interface QuestionRepository {
     suspend fun getQuestions(): List<Question>
 }
