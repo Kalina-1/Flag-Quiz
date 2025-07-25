c2c30e65d94dfd396a900251b777bc1776b3fe9f
Kalina-1 <kalinashrestha16@gmail.com>
2025-07-25 19:48:18 +0545

    first name updated

--- app/src/main/java/com/example/flagquiz/model/UserModel.kt
+++ app/src/main/java/com/example/flagquiz/model/UserModel.kt
@@ -6,7 +6,7 @@ data class User(
     var uid: String? = null, // The unique identifier for the user
     var username: String? = null, // The username chosen by the user
     var email: String? = null, // The user's email address
-    var firstName: String? = null,
+    var firstName: String? = null, // The user's first name
     var lastName: String? = null,
     var address: String? = null
 )
