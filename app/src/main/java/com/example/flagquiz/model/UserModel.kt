49f048ebd91fe4af6c80c4067f585a051e7ab3c0
Kalina-1 <kalinashrestha16@gmail.com>
2025-07-25 19:47:37 +0545

    email address updated

--- app/src/main/java/com/example/flagquiz/model/UserModel.kt
+++ app/src/main/java/com/example/flagquiz/model/UserModel.kt
@@ -5,7 +5,7 @@ package com.example.flagquiz.model
 data class User(
     var uid: String? = null, // The unique identifier for the user
     var username: String? = null, // The username chosen by the user
-    var email: String? = null,
+    var email: String? = null, // The user's email address
     var firstName: String? = null,
     var lastName: String? = null,
     var address: String? = null
