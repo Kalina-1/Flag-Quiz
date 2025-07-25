7e47ee26f4cf8fb28f3af40931336b0eb97412c4
Kalina-1 <kalinashrestha16@gmail.com>
2025-07-25 19:41:46 +0545

    hint about capital city added

--- app/src/main/java/com/example/flagquiz/model/Question.kt
+++ app/src/main/java/com/example/flagquiz/model/Question.kt
@@ -3,6 +3,7 @@ package com.example.flagquiz.model
 // This class represents a quiz question.
 // It contains information about the flag image,
 // a list of possible answers, the correct answer,
+// and a hint about the capital city of the country represented by the flag.
 // the correct answer itself, and a little hint about the capital city.
 // Makes it easy to pass around all question details together.
 data class Question(
