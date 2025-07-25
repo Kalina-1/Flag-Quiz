4b98042a90666e65d6a0c350bd2dd82b6acadb36
Kalina-1 <kalinashrestha16@gmail.com>
2025-07-25 19:45:52 +0545

    uniques identifier for user updated

--- app/src/main/java/com/example/flagquiz/model/UserModel.kt
+++ app/src/main/java/com/example/flagquiz/model/UserModel.kt
@@ -3,7 +3,7 @@ package com.example.flagquiz.model
 // This class holds information about a user in the app.
 
 data class User(
-    var uid: String? = null,
+    var uid: String? = null, // The unique identifier for the user
     var username: String? = null,
     var email: String? = null,
     var firstName: String? = null,
